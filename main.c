bd0c46ab794bb3bc8f7772557b59959ecba031b4
wuqin04 <wuqin419@gmail.com>
2025-09-07 16:00:29 +0800

    Added random generator

--- main.c
+++ main.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
 #include <time.h>
 
+long int rand(void);
+
+void srand(unsigned int seed);
+
+int getRandomInt();
+
 int main() {
     int length = 0;
     printf("Enter the length of your password: ");
     scanf("%d", &length);
-    
-    srand(time(NULL));
+    char password[length];
 
-    int randomize = (rand() % 122) + 97;
-    char password = randomize;
-    printf("Your password is: %c", password);
+    for (int i = 0; i < length; i++) {
+        password[i] = getRandomInt();
+    }
 }
+
+int getRandomInt() {
+    srand(((unsigned int)time(NULL)));
+    const int max = 122;
+    const int min = 92;
+    int random = (rand() % (max - min + 1)) + min;
+    return random;
+}
+
