418970b86a8d0c52e6b5dd3b9a2754727bf9cb44
wuqin04 <wuqin419@gmail.com>
2025-09-07 18:13:24 +0800

    Final ver

--- main.c
+++ main.c
@@ -6,18 +6,43 @@
 char getRandomChar();
 
 int main() {
-    int len = 0;
-    char* password = "empty";
-    printf("Enter the length of your password: ");
-    scanf("%d", &len);
+    srand(((unsigned int)time(NULL)));
 
-    printf("Your random character is: %s\n", password);
+    int len;
+    char term;
+    char* password = NULL;
+
+    printf("Enter the length of your password: ");
+    if (scanf("%d%c", &len, &term) != 2 || term != '\n') {
+        fputs("error: the input is not integer, exiting.\n", stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    if (len < 0) {
+        fputs("error: the length cannot be smaller than 0, exiting.\n", stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    // alloc max size for password
+    password = malloc(sizeof(char)*len + 1);
+    if (!password) {
+        fputs("error: password allocation failed, exiting.\n", stderr);
+        exit(EXIT_FAILURE);
+    }
+    password[len] = '\0';
+
+    for (size_t i = 0; i < len; i++) {
+        password[i] = getRandomChar();
+    }
+
+    printf("The generated password is: %s\n", password);
+
+    free(password);
 }
 
 char getRandomChar() {
-    srand(((unsigned int)time(NULL)));
-    const int max = 122;
-    const int min = 92;
-    long int random = (rand() % (max - min + 1)) + min;
+    const int max = 126;
+    const int min = 33;
+    int random = (rand() % (max - min + 1)) + min;
     return (char)random;
 }
